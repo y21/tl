cd431420ef86cf4e30be65892b1dd9b7254884b8
y21 <btimo8822@gmail.com>
2021-03-21 14:23:00 +0100

    use range for checking identifier

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 pub fn is_ident(c: u8) -> bool {
-    (c >= b'0' && c <= b'9')
+    (b'0'..=b'9').contains(&c)
         || (b'A'..=b'Z').contains(&c)
         || (b'a'..=b'z').contains(&c)
         || c == b'-'
