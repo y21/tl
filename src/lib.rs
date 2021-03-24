0ff5c9ce72d4abde3830ef4ae89313da8149181a
y21 <btimo8822@gmail.com>
2021-03-24 09:21:35 +0100

    lib: export parse function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-mod util;
-mod stream;
-mod parser;
 mod bytes;
-
+mod parser;
+mod stream;
+mod util;
+mod vdom;
 #[cfg(test)]
-mod tests {
-    
+mod tests;
+
+use parser::Parser;
+use vdom::VDom;
+
+pub fn parse(input: &str) -> VDom<'_> {
+    VDom::from(Parser::new(input).parse())
 }
