da95ea1890a76fa5285d0eb2e034292f65915f00
y21 <btimo8822@gmail.com>
2021-03-24 12:16:08 +0100

    bytes: rename to `Bytes`

--- src/bytes.rs
+++ src/bytes.rs
@@ -2,49 +2,53 @@ use core::{fmt, fmt::Debug};
 use std::borrow::Cow;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
-pub struct BorrowedBytes<'a>(pub &'a [u8]);
+pub struct Bytes<'a>(&'a [u8]);
 
-impl<'a> From<&'a str> for BorrowedBytes<'a> {
+impl<'a> From<&'a str> for Bytes<'a> {
     fn from(s: &'a str) -> Self {
         Self(s.as_bytes())
     }
 }
 
-impl<'a> From<&'a [u8]> for BorrowedBytes<'a> {
+impl<'a> From<&'a [u8]> for Bytes<'a> {
     fn from(s: &'a [u8]) -> Self {
         Self(s)
     }
 }
 
-impl<'a> Debug for BorrowedBytes<'a> {
+impl<'a> Debug for Bytes<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("BorrowedBytes")
+        f.debug_tuple("Bytes")
             .field(&self.as_utf8_str())
             .finish()
     }
 }
 
-impl<'a> BorrowedBytes<'a> {
+impl<'a> Bytes<'a> {
     pub fn as_utf8_str(&self) -> Cow<'a, str> {
         String::from_utf8_lossy(self.0)
+    }
+
+    pub fn raw(&self) -> &'a [u8] {
+        self.0
     }
 }
 
 pub trait AsBytes {
-    fn as_bytes<'a>(&'a self) -> BorrowedBytes<'a>;
+    fn as_bytes<'a>(&'a self) -> Bytes<'a>;
 }
 
 impl AsBytes for String {
-    fn as_bytes(&self) -> BorrowedBytes<'_> {
-        BorrowedBytes::from(&self[..])
+    fn as_bytes(&self) -> Bytes<'_> {
+        Bytes::from(&self[..])
     }
 }
 
 macro_rules! asbytes_from_impl {
     ($t:ty) => {
         impl AsBytes for $t {
-            fn as_bytes(&self) -> BorrowedBytes<'_> {
-                BorrowedBytes::from(self)
+            fn as_bytes(&self) -> Bytes<'_> {
+                Bytes::from(self)
             }
         }
     };
